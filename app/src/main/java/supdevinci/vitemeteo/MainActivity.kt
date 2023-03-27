8500df070fcd45f860c38dcdd0f54ececaa6ed6d
Marc-Antoine <marcantoine.leonelli@gmail.com>
2023-03-27 16:35:36 +0200

    initial commit

---
+++ app/src/main/java/supdevinci/vitemeteo/MainActivity.kt
@@ -0,0 +1,11 @@
+package supdevinci.vitemeteo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
